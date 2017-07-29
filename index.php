923a9dc65f4bf480b66ebe247c7acb7631ed620c
Sergii Bubalo <sergii.bubalo@gmail.com>
2017-07-29 21:49:28 +0300

    Initial commit

---
+++ index.php
@@ -0,0 +1,44 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+    <link rel="stylesheet" href="styles/style.css">
+</head>
+<body>
+
+</body>
+<div class="container">
+    <header class="header">
+        <div class="logo">
+            Logo
+        </div>
+        <nav class="nav">
+            <a href="#">First</a>
+            <a href="#">Second</a>
+            <a href="#">Third</a>
+            <a href="#">Fourth</a>
+            <i>Settings</i>
+        </nav>
+        <!-- /.logo -->
+    </header>
+    <aside class="sidebar">
+        Sidebar
+    </aside>
+    <!-- /.sidebar -->
+    <section class="main">
+        Main content
+    </section>
+    <!-- /.main -->
+    <div class="clearfix"></div>
+    <!-- /.ckearfix -->
+    <footer class="footer">
+        <div class="footer-block">
+            Footer 1
+        </div>
+        <div class="footer-block">
+            Footer 2
+        </div>
+    </footer>
+</div>
+<!-- /.container -->
+</html>
