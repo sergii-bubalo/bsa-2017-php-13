6ec8c4a2c1ea72a8eebc7d8cd7ff248b591583a0
Sergii Bubalo <sergii.bubalo@gmail.com>
2017-07-30 13:54:03 +0300

    Changes in markup && CSS

--- index.php
+++ index.php
@@ -1,8 +1,9 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Document</title>
+    <title>BSA | HTML/CSS</title>
     <link rel="stylesheet" href="styles/style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
 <body>
 
@@ -17,26 +18,89 @@
             <a href="#">Second</a>
             <a href="#">Third</a>
             <a href="#">Fourth</a>
-            <i>Settings</i>
+            <i class="fa fa-cogs" aria-hidden="true"></i>
         </nav>
         <!-- /.logo -->
     </header>
-    <aside class="sidebar">
-        Sidebar
-    </aside>
-    <!-- /.sidebar -->
-    <section class="main">
-        Main content
-    </section>
+    <div class="content-container">
+        <aside class="sidebar">
+            <ul>
+                <li><a href="#">1st item</a></li>
+                <li><a href="#">2st item</a></li>
+                <li><a href="#">3st item</a></li>
+                <li><a href="#">4st item</a></li>
+                <li><a href="#">5st item</a></li>
+            </ul>
+        </aside>
+        <!-- /.sidebar -->
+        <section class="main">
+            <div class="content-block">
+                Main content 1Main content 1Main content 1Main content 1Main content 1Main content 1Main content 1Main content 1Main content 1Main content 1Main content 1Main content 1Main content 1
+            </div>
+            <div class="content-block">
+                Main content 2Main content 2Main content 2Main content 2Main content 2
+            </div>
+            <div class="content-block">
+                Main content 3Main content 3Main content 3Main content 3Main content 3Main content 3Main content 3Main content 3Main content 3Main content 3Main content 3Main content 3Main content 3Main content 3Main content 3Main content 3Main content 3Main content 3Main content 3Main content 3Main content 3Main content 3
+            </div>
+            <div class="content-block">
+                Main content 4Main content 4Main content 4Main content 4Main content 4Main content 4Main content 4Main content 4Main content 4Main content 4Main content 4Main content 4Main content 4Main content 4Main content 4
+            </div>
+            <div class="content-block">Main content 5</div>
+            <div class="content-block">Main content 6</div>
+            <div class="content-block">Main content 7</div>
+            <div class="content-block">Main content 8</div>
+            <div class="content-block">Main content 9</div>
+        </section>
+    </div>
     <!-- /.main -->
-    <div class="clearfix"></div>
-    <!-- /.ckearfix -->
-    <footer class="footer">
-        <div class="footer-block">
-            Footer 1
+    <footer class="footer black-border">
+        <div class="footer-block-1">
+            <div>
+                <ul>
+                    <li><a href="#">1st item</a></li>
+                    <li><a href="#">2st item</a></li>
+                    <li><a href="#">3st item</a></li>
+                    <li><a href="#">4st item</a></li>
+                    <li><a href="#">5st item</a></li>
+                </ul>
+            </div>
+            <div>
+                <ul>
+                    <li><a href="#">1st item</a></li>
+                    <li><a href="#">2st item</a></li>
+                    <li><a href="#">3st item</a></li>
+                    <li><a href="#">4st item</a></li>
+                </ul>
+            </div>
+            <div>
+                <ul>
+                    <li><a href="#">1st item</a></li>
+                    <li><a href="#">2st item</a></li>
+                    <li><a href="#">3st item</a></li>
+                </ul>
+            </div>
+            <div>
+                <ul>
+                    <li><a href="#">1st item</a></li>
+                    <li><a href="#">2st item</a></li>
+                    <li><a href="#">3st item</a></li>
+                    <li><a href="#">4st item</a></li>
+                    <li><a href="#">5st item</a></li>
+                </ul>
+            </div>
         </div>
-        <div class="footer-block">
-            Footer 2
+        <div class="footer-block-2">
+            <div>
+                <a href="">1st</a>
+                <a href="">2nd</a>
+                <a href="">3rd</a>
+            </div>
+            <div>
+                <a href="https://facebook.com" target="_blank"><i class="fa fa-facebook"></i></a>
+                <a href="https://linkedin.com" target="_blank"><i class="fa fa-linkedin"></i></a>
+                <a href="https://plus.google.com" target="_blank"><i class="fa fa-google-plus"></i></a>
+            </div>
         </div>
     </footer>
 </div>
